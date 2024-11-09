0755066ddef994b09bfd7e6d5012cf2d372406d2
Lazizbek <mywave.17@gmail.com>
2024-11-09 10:38:33 +0100

    first commit

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -9,15 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardWiev(isFacedUp: true)
+            CardWiev()
+            CardWiev()
+            CardWiev()
+            
         }
         .padding()
+        
     }
 }
+struct CardWiev :View {
+    var isFacedUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFacedUp {
+                RoundedRectangle(cornerRadius: 20)
+                               .foregroundColor(.orange)
+                           RoundedRectangle(cornerRadius: 19)
+                               .strokeBorder(lineWidth : 2)
+                           Text("🤖").font(.largeTitle)
+            }
+            else    {
+                RoundedRectangle(cornerRadius: 20)
+                               .foregroundColor(.orange)
+            }
+           
+                
+                
+              
+                
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
