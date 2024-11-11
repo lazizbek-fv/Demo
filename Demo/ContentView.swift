2519bdcd08ea77301834df9061039b97b71eba46
Lazizbek <mywave.17@gmail.com>
2024-11-11 16:30:11 +0100

    finished second lesson

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -8,33 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🕷️","🎃","😈","🤖","👻","🧙‍♀️","🧛‍♂️","🧟‍♀️","🕷️","🎃","😈","🤖"]
+    let emojis = ["🕷️", "🎃", "😈", "🤖", "👻", "🧙‍♀️", "🧛‍♂️", "🧟‍♀️", "🕷️", "🎃", "😈", "🤖","🕷️", "🎃", "😈", "🤖", "👻", "🧙‍♀️", "🧛‍♂️", "🧟‍♀️", "🕷️", "🎃", "😈", "🤖"]
     @State var cardCount: Int = 4
     var body: some View {
-        VStack{
-            HStack {
-                ForEach(0..<cardCount, id: \.self) { index in
-                    CardWiev(content: emojis[index])
-                    }
+        VStack {
+            ScrollView {
+                cards
             }
+            
+            Spacer()
             HStack {
-                Button("Add Card") {
-                    if cardCount < emojis.count {
-                        cardCount += 1
-                    }
-                
-                }
+                cardAdder
                 Spacer()
-                Button("Remove Card") {
-                    if cardCount > 1 {
-                        cardCount -= 1
-                    }
-                    
-                }
+                cardRemove
             }
         }
-       
         .padding()
+    }
+
+    func cardCountAdjustment(by count: Int, name: String) -> some View {
+        Button(name) {
+            cardCount += count
+        }
+        .disabled(cardCount + count < 1 || cardCount + count > emojis.count)
+    }
+
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+            ForEach(0 ..< cardCount, id: \.self) { index in
+                CardWiev(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+    }
+
+    var cardAdder: some View {
+        cardCountAdjustment(by: -1, name: "Remove Card")
+    }
+
+    var cardRemove: some View {
+        cardCountAdjustment(by: 1, name: "Add Card")
     }
 }
 
@@ -44,14 +57,13 @@ struct CardWiev: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 20)
-            if isFacedUp {
-
+            Group {
                 base.foregroundColor(.orange)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.foregroundColor(.yellow)
-            }
+            }.opacity(isFacedUp ? 1 : 0)
+            base.fill().opacity(isFacedUp ? 0 : 1).foregroundStyle(.orange)
+            base.strokeBorder(lineWidth: 2)
         }
         .onTapGesture {
             print("tapped")
